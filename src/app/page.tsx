245884b62e9c448218e49616014b4cb5cd66e63f
Muh-Uzair <muhammaduzair1062001@gmail.com>
2025-04-06 16:19:22 +0500

    styles in seperate folder

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const page = () => {
-  return <div className="bg-sky-500 tab:bg-yellow-200">hello</div>;
+  return <div className="bg-sky-500 tab:bg-yellow-200 font-medium">Roboto</div>;
 };
 
 export default page;
